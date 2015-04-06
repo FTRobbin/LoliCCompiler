9c1f46dd3ba1f74b6104bbd372e806be44c3933e
Necrololicon <TankEngineer@yandex.com>
2015-04-06 13:30:19 +0800

    Created Main and ParserTest

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Created by Robbin Ni on 2015/4/6.
+ */
+
+public class Main {
+
+    public static String pathOf(String filename) { return Main.class.getResource(filename).getPath(); }
+
+    private static void compile(String filename) {
+    }
+
+    public static void main(String argv[]) {
+    }
+}
