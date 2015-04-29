fb651ad66b05e612c0f83ab47db5c3e934ed4932
Necrololicon <TankEngineer@yandex.com>
2015-04-29 23:03:34 +0800

    For midterm.

---
+++ src/main/Midterm.java
@@ -0,0 +1,34 @@
+package main;
+
+import ast.nodes.Program;
+import parser.Parser;
+import semantic.IRTBuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ * Created by Robbin Ni on 2015/4/29.
+ */
+public class Midterm {
+
+    static public void main(String argv[]) {
+        Reader reader = new BufferedReader(new InputStreamReader(System.in));
+        Parser parser = new Parser(reader);
+        Program prog = null;
+        try {
+            parser = new Parser(reader);
+            prog = (Program)parser.parse().value;
+        } catch (Throwable t) {
+            System.exit(1);
+        }
+        IRTBuilder builder = new IRTBuilder();
+        try {
+            prog.accept(builder);
+        } catch (Throwable t) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+}
