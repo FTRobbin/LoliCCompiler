00c97f1157eb61d4b6b04982174b8a678bd2d340
Necrololicon <TankEngineer@yandex.com>
2015-05-23 07:12:07 +0800

    Small Change

--- src/gen/SPIMCode.java
+++ src/gen/SPIMCode.java
@@ -1,42 +1,9 @@
 package gen;
 
-import java.util.LinkedList;
-import java.util.List;
+import gen.ASMCode;
 
 /**
- * Created by Robbin Ni on 2015/5/19.
+ * Created by Robbin Ni on 2015/5/23.
  */
 public class SPIMCode extends ASMCode {
-
-    public List<String> data;
-
-    public List<String> text;
-
-    public SPIMCode() {
-        data = new LinkedList<>();
-        data.add("\t.data");
-        text = new LinkedList<>();
-        text.add("\t.text");
-        text.add("\t.globl main");
-    }
-
-    public void addData(String inst) {
-        data.add(inst);
-    }
-
-    public void addText(String inst) {
-        text.add(inst);
-    }
-
-    @Override
-    public String print() {
-        String ret = "";
-        for (String s : data) {
-            ret += s + "\n";
-        }
-        for (String s : text) {
-            ret += s + "\n";
-        }
-        return ret;
-    }
 }
---
+++ src/gen/basic/BasicSPIMCode.java
@@ -0,0 +1,44 @@
+package gen.basic;
+
+import gen.ASMCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Robbin Ni on 2015/5/19.
+ */
+public class BasicSPIMCode extends ASMCode {
+
+    public List<String> data;
+
+    public List<String> text;
+
+    public BasicSPIMCode() {
+        data = new LinkedList<>();
+        data.add("\t.data");
+        text = new LinkedList<>();
+        text.add("\t.text");
+        text.add("\t.globl main");
+    }
+
+    public void addData(String inst) {
+        data.add(inst);
+    }
+
+    public void addText(String inst) {
+        text.add(inst);
+    }
+
+    @Override
+    public String print() {
+        String ret = "";
+        for (String s : data) {
+            ret += s + "\n";
+        }
+        for (String s : text) {
+            ret += s + "\n";
+        }
+        return ret;
+    }
+}
