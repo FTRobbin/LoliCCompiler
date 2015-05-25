7bf292569909a6f3d504218f9c6f75b9423ba334
Necrololicon <TankEngineer@yandex.com>
2015-05-25 22:56:03 +0800

    IR small improvement. Bug fixes.

--- src/irt/factory/UniIncIntOp.java
+++ src/irt/factory/UniIncIntOp.java
@@ -30,15 +30,14 @@ public class UniIncIntOp extends Op {
 
     @Override
     public Value genIR(Label cur, List<MIRInst> list, Label next, MIRGen gen, VarName ret) {
-        if (ret == null || ret.isAbsTmp()) {
-            Label tcur = new Label(Label.DUMMY);
-            VarName dest = (VarName)gen.gen(cur, expr.exprs.get(0), list, tcur, VarName.getAbsTmp());
-            list.add((new AssignInst(delta < 0 ? ExprOp.sub : ExprOp.add, dest, dest, new IntConst(Math.abs(delta)))).setLabel(tcur));
+        Label tcur = new Label(Label.DUMMY);
+        VarName dest = (VarName)gen.gen(cur, expr.exprs.get(0), list, tcur, VarName.getAbsTmp());
+        list.add((new AssignInst(delta < 0 ? ExprOp.sub : ExprOp.add, dest, dest, new IntConst(Math.abs(delta)))).setLabel(tcur));
+        if (ret != null && !ret.isAbsTmp()) {
+            list.add(new AssignInst(ExprOp.asg, ret, dest));
+            return ret;
+        } else {
             return dest;
         }
-        Label tcur = new Label(Label.DUMMY);
-        VarName dest = (VarName)gen.gen(cur, expr.exprs.get(0), list, tcur, ret);
-        list.add((new AssignInst(delta < 0 ? ExprOp.sub : ExprOp.add, dest, dest, new IntConst(Math.abs(delta)))).setLabel(tcur));
-        return dest;
     }
 }
